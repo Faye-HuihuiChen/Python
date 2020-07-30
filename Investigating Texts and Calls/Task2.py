c3e5574dd0532beb91d2dc39c3a260e838c8281d
Faye-HuihuiChen <68520716+Faye-HuihuiChen@users.noreply.github.com>
2020-07-29 22:21:13 -0400

    Update Task2.py

--- Investigating Texts and Calls/Task2.py
+++ Investigating Texts and Calls/Task2.py
@@ -4,14 +4,13 @@ It's ok if you don't understand how to read files
 """
 from collections import defaultdict
 import csv
-from datetime import datetime
-with open('/Users/chenhuihui/Desktop/texts.csv', 'r') as f:
+with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
-with open('/Users/chenhuihui/Desktop/calls.csv', 'r') as f:
+
+with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
-
 """
 TASK 2: Which telephone number spent the longest time on the phone
 during the period? Don't forget that time spent answering a call is
