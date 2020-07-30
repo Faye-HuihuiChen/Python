2e1d66568b3a3732f004b60c1e913ef061b314d9
Faye-HuihuiChen <68520716+Faye-HuihuiChen@users.noreply.github.com>
2020-07-29 22:21:45 -0400

    Update Task3.py

--- Investigating Texts and Calls/Task3.py
+++ Investigating Texts and Calls/Task3.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-
-with open('/Users/chenhuihui/Desktop/texts.csv', 'r') as f:
+with open('texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
-with open('/Users/chenhuihui/Desktop/calls.csv', 'r') as f:
+
+with open('calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 """
